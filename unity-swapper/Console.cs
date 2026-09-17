5c5fb78231ee152d928594ba0e77a589321b48f4
apierce <pierceaaron424@gmail.com>
2026-09-16 21:56:34 -0500

    Finish base functionality

--- unity-swapper/Console.cs
+++ unity-swapper/Console.cs
@@ -7,15 +7,33 @@ class CliApp
 {
     static void Main(string[] args)
     {
-        /*
         if (!OperatingSystem.IsMacOS())
         {
             Console.WriteLine("Unity swapper is only supported on MacOS");
             Environment.Exit(1);
         }
-        */
+        
+        //Get game path
+        var appPath = getInput("Please enter the path to the 32 bit game .app you want to swap:");
+        
+        //Get Unity player path
+        
+        //Merge Directories
+        
+        //Done
+    }
 
-        FileUtilities.MergeDirectories("/home/apierce/Desktop/from/test", "/home/apierce/Desktop/to/test");
+    private static string getInput(string prompt)
+    {
+        Console.WriteLine(prompt);
+        var input = string.Empty;
+
+        while (input == string.Empty)
+        {
+            input = Console.ReadLine();
+        }
+
+        return input;
     }
 
     private static string SendToBash(string input)
